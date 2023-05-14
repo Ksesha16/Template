942241395b57434cd8917647b17be45be2ed9848
Ksusha4337 <FedotovaKA@stud.kai.ru>
2023-05-15 00:25:56 +0300

    Добавили кнопку и создали обработчик

--- Template_4337/MainWindow.xaml.cs
+++ Template_4337/MainWindow.xaml.cs
@@ -30,5 +30,10 @@ namespace Template_4337
             var window = new Khuzyakaev_4337();
             window.Show();
         }
+
+        private void Fedotova_4337_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
